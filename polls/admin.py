754b313e0db59bfdbd904a0d8f06308c010f018c
Biff <thedonnieutley@gmail.com>
2017-05-10 11:20:30 -0500

    Finished basics...

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,26 @@ from django.contrib import admin
 from .models import Question, Choice
 
 # Register your models here.
-admin.site.register(Question)
 
-admin.site.register(Choice)
+#admin.site.register(Choice)
+
+#class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+  model = Choice
+  extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+  #fields = ['pub_date', 'question_text']
+  fieldsets = [
+    (None,							{'fields': ['question_text']}),
+    ('Date Information',{'fields': ['pub_date']}),
+  ]
+  inlines = [ChoiceInline]
+  list_display = ('question_text', 'pub_date', 'was_published_recently')
+  list_filter = ['pub_date']
+  search_fields = ['question_text']
+
+  #list_per_page = 2 # Pagination defaults to 100/page if not defined
+
+admin.site.register(Question, QuestionAdmin)
+
